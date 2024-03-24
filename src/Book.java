7be4965f3a6d7f600924f39a5714ad6a2447b272
Максим <bogdanoff.96@yandex.ru>
2024-03-24 12:41:01 +1100

    Completed

---
+++ src/Book.java
@@ -0,0 +1,30 @@
+public class Book {
+
+    private String bookName;
+    private Author author;
+    private int bookAge;
+
+    public Book(String bookName, Author author, int bookAge) {
+        this.bookName = bookName;
+        this.author = author;
+        this.bookAge = bookAge;
+    }
+
+    public String getBookName() {
+        return this.bookName;
+    }
+
+    public int getBookAge() {
+        return this.bookAge;
+    }
+
+    public Author getAuthor() {
+        return this.author;
+    }
+
+    public void setBookAge(int bookAge) {
+        this.bookAge = bookAge;
+    }
+
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,12 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Author bookOne = new Author("Федор", "Достоевский");
+        Author bookTwo = new Author("Даниель", "Дефо");
+        Book one = new Book("Преступление и наказание", bookOne, 1866);
+        Book two = new Book("Робинзон Крузо", bookTwo, 1719);
+        System.out.println("Название книги " + one.getBookName() + ". Автор " + one.getAuthor() + " , год выхода " + one.getBookAge());
+        System.out.println("Название книги " + two.getBookName() + ". Автор " + two.getAuthor() + " , год выхода " + two.getBookAge());
     }
 }
+
